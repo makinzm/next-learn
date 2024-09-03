be6c6ec72d15e6b20b0e987695e8699750e7ad77
makinzm <nozomi.maki.da@gmail.com>
2024-09-03 20:48:42 +0900

    add font

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import mystyles from '@/app/ui/home.module.css';
+import { nerkoOne } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
@@ -10,14 +11,14 @@ export default function Page() {
     // Ref: [Min-Height - Tailwind CSS](https://tailwindcss.com/docs/min-height)
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div
             className={mystyles.shape}
           />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${nerkoOne.className} text-lg text-gray-800`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Nerko_One } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const nerkoOne = Nerko_One({ subsets: ['latin'], weight: "400" });

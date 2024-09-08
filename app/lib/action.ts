8fbab6f13d4cfbe409a70f71950d2018f0d02175
makinzm <nozomi.maki.da@gmail.com>
2024-09-08 21:15:02 +0900

    feat: insert data

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,3 +1,40 @@
-'use server'
+'use server';
 
-export async function createInvoice(formData: FormData) {}
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// TODO: Add revalidate amount to avoid overflows
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['paid', 'pending']),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true});
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  // Test it out:
+  console.log(typeof amount);
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Revalidate the invoices page to show the new invoice
+  revalidatePath('/dashboard/invoices');
+  // Back to the invoices page
+  redirect('/dashboard/invoices');
+}
+

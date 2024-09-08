64afc0e5d459b794051000dff10b2800384a26dc
makinzm <nozomi.maki.da@gmail.com>
2024-09-08 22:07:37 +0900

    bug: intentional bug

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -70,6 +70,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
